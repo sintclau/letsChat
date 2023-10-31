19edfa8c4bfcace873f61e2fd9396dc69b913c3a
Claudiu Spiescu <claudiuspiescu@gmail.com>
2023-10-31 12:27:01 +0200

    Added login checking

--- client/src/database/dbCheckUser.js
+++ client/src/database/dbCheckUser.js
@@ -1,18 +1,42 @@
 import db from "./dbconfig";
 
-function dbCheckUser(username, password) {
-    const passwordChecker = db.searchByValue(
-        {
-            schema: 'auth',
-            table: 'users',
-            searchAttribute: 'username',
-            searchValue: username,
-            attributes: ['password']
+async function dbCheckUser(username, password) {
+    console.log("checking...");
+    const userPassword = await passwordChecker(username);
+    if (userPassword === false) {
+        return false;
+    } else {
+        if (password === userPassword) {
+            return true;
+        } else {
+            return false;
         }
-    );
-    if (password == passwordChecker) {
-        return true;
-    } else return false;
+    }
+}
+
+async function passwordChecker(username) {
+    return new Promise((resolve, reject) => {
+        db.searchByValue(
+            {
+                schema: 'auth',
+                table: 'users',
+                searchAttribute: 'username',
+                searchValue: username,
+                attributes: ['password']
+            },
+            (err, res) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (res.data.length === 0) {
+                        resolve(false);
+                    } else {
+                        resolve(res.data[0].password);
+                    };
+                }
+            }
+        );
+    });
 }
 
 export default dbCheckUser;
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -2,20 +2,21 @@ import dbCheckUser from '../../database/dbCheckUser';
 import styles from './styles.module.css';
 
 const Home = ({ username, setUsername, password, setPassword, socket }) => {
-    function login(username, password) {
-            console.log("test");
-            if (dbCheckUser(username, password) == true) {
-                console.log("Logged in succesfully!");
-            }
+    const handleLogin = async () => {
+        const userCheck = await dbCheckUser(username, password);
+        if (userCheck) {
+            console.log("Logged in successfully!");
+        } else {
+            console.log("Couldn't log in!");
+        }
     };
-
     return (
         <div className={styles.container}>
             <div className={styles.formContainer}>
                 <h1>{`letsChat`}</h1>
                 <input className={styles.userInput} placeholder='Username' onChange={(e) => setUsername(e.target.value)} />
                 <input className={styles.passInput} type='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
-                <button className='btn btn-primary' onClick={login(username, password)}>Login</button>
+                <button className='btn btn-primary' onClick={handleLogin}>Login</button>
             </div>
             <div className={styles.rightContainer} />
         </div>
